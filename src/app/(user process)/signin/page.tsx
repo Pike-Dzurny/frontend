8a3bcc4d97fc09ae0d52d54c5b2515b5cace6bbf
Pike-Dzurny <pike.m.dzurny@gmail.com>
2023-11-23 20:39:40 -0600

    Update package.json and signin page

--- src/app/(user process)/signin/page.tsx
+++ src/app/(user process)/signin/page.tsx
@@ -1,19 +1,40 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation'
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { replace } from 'lodash';
+
+
 
 export default function SignIn() {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const username = formData.get('username');
-    const password = formData.get('password');
-  
-    await signIn('credentials', { username, password, callbackUrl: '/' });
-  };
 
-  return (
+
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter()
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const username = formData.get('username');
+        const password = formData.get('password');
+    
+        if (username && password) {
+            const result = await signIn('credentials', { redirect: false, username, password });
+            if (result?.ok && result.url) {
+                console.log(`Result_URL: ${result.url} | Result_OK: ${result.ok} | Result: ${result}`);
+                router.push('/');
+            } else {
+              setError('Sorry, your password was incorrect. Please double-check your password.');
+            }
+          } else {
+            setError('Username and password are required.');
+          }
+      };
+  
+    return (
     <div className="flex flex-col md:flex-row items-center justify-center min-h-screen py-2">
       <div className="hidden md:block md:w-2/3">
         <img src="https://extension.unh.edu/sites/default/files/styles/max_width_480px/public/migrated_images/trees.jpg?itok=b-gZXROM" alt="Placeholder" className="object-cover w-full h-full" />
@@ -25,6 +46,9 @@ export default function SignIn() {
           <input name="username" placeholder="Username" required className="mb-4 w-full p-2 border-2 border-gray-300 rounded-md" />
           <input name="password" type="password" placeholder="Password" required className="mb-4 w-full p-2 border-2 border-gray-300 rounded-md" />
           <button type="submit" className="mb-4 w-full p-2 bg-sky-500 hover:bg-sky-400 text-white rounded-md">Sign In</button>
+          <hr className='p-2'/>         
+          {error && <div className="text-center mx-auto text-red-500">{error}</div>}
+
           <Link href="/forgot-password">
             <p className="text-sky-500 text-center hover:text-sky-500">Forgot Password?</p>
           </Link>
